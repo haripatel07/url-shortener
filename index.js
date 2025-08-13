1b516248dd43eb1abfbba7ffe32889184c296def
Hari Patel <patelhariv18@gmail.com>
2025-08-13 21:47:59 +0530

    Added basic frontend for basic user interaction

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-// This is the entry point for the URL shortener application
+// This is the final entry point for the URL shortener application
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./db');
+
+// Route files
+const authRoutes = require('./routes/auth');
 const urlRoutes = require('./routes/url');
+const analyticsRoutes = require('./routes/analytics');
 const redirectRoutes = require('./routes/redirect');
 
 dotenv.config();
 
 const app = express();
+
+// Connect to the database
 connectDB();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+// get the public directory for static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the routes
-app.use('/api', urlRoutes);
+const PORT = process.env.PORT || 3000;
+const API_PREFIX = '/api/v1';
+
+// Use the routes with a common API prefix
+app.use(`${API_PREFIX}/auth`, authRoutes);
+app.use(`${API_PREFIX}/url`, urlRoutes);
+app.use(`${API_PREFIX}/analytics`, analyticsRoutes);
+
+
 app.use('/', redirectRoutes);
 
 app.listen(PORT, () => {
