16de5d05c9a428a691769692234b4333703e04c8
Hari Patel <patelhariv18@gmail.com>
2025-08-13 09:56:31 +0530

    Made changes to url model

--- models/Url.js
+++ models/Url.js
@@ -2,24 +2,31 @@
 const mongoose = require('mongoose');
 
 const urlSchema = new mongoose.Schema({
-    originalUrl:{
-        type: String,
-        required: true
-    },
-    shortUrl:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    clicks:{
-        type: Number,
-        default: 0
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-
-})
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+  expiresAt: {
+    type: Date,
+    default: null,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+  },
+  analytics: {
+    type: Array,
+    default: [],
+  },
+});
 
 module.exports = mongoose.model('Url', urlSchema);
