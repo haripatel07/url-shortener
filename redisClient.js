ca629dff30c5405512c28a098a5aee40554f59d3
Hari Patel <patelhariv18@gmail.com>
2025-08-13 21:05:19 +0530

    Added authentication using json web token

--- redisClient.js
+++ redisClient.js
@@ -1,11 +1,10 @@
-// redisClient.js
 const redis = require('redis');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379'
+  url: process.env.REDIS_URL
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
