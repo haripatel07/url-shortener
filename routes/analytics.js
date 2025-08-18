692134d4579874c1189009fee5cc780c9cb5c5e7
Hari Patel <patelhariv18@gmail.com>
2025-08-18 22:25:01 +0530

    Added pictures folder and made changes to analytics.js

--- routes/analytics.js
+++ routes/analytics.js
@@ -7,14 +7,20 @@ const router = express.Router();
 
 router.get('/:shortUrl', auth, async (req, res) => {
   try {
-    const url = await Url.findOne({ shortUrl: req.params.shortUrl, userId: req.user.id });
+    const url = await Url.findOne({
+      shortUrl: req.params.shortUrl,
+      userId: req.user._id,
+    });
 
     if (!url) {
       return res.status(404).json({ msg: 'URL not found or not owned by user' });
     }
 
-    res.json(url.analytics);
+    res.status(200).json({
+      analytics: url.analytics, 
+    });
   } catch (error) {
+    console.error(error.message);
     res.status(500).json({ msg: 'Server error' });
   }
 });
